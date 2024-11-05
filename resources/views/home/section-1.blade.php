1a6d7118c99fbdbde0b50771342258c28ea79b13
Bayu Krisna <bayukrisna231@gmail.com>
2024-11-05 23:20:42 +0700

    pembaruan section home

--- resources/views/home/section-1.blade.php
+++ resources/views/home/section-1.blade.php
@@ -1,5 +1,5 @@
-<div class="min-h-screen flex items-center justify-between mx-20 relative">
-    <div class="flex flex-col gap-5 max-w-2xl">
+<div class="flex justify-between mx-20 relative">
+    <div class="flex flex-col gap-5 max-w-2xl mt-48">
       <p class="text-5xl">Knowledge Connection</p>
       <p class="text-5xl font-bold">Open the Door to the Future</p>
       <div>
@@ -12,11 +12,11 @@
           student in our learning community.
         </p>
       </div>
-      <button class="w-48 bg-yellow-500 text-white p-3 rounded-xl shadow-lg hover:bg-yellow-600">
+      <button class="w-64 bg-yellow-500 text-white p-5 rounded-xl text-xl shadow-lg hover:bg-yellow-600">
         Get started!
       </button>
     </div>
-    <p class="bg-pink-100 w-96 h-96 text-white p-3 rounded-xl absolute right-0 "></p>
-    <img src="img/kids.png" alt="Kids reading book" class="absolute right-0 max-w-md h-auto">
+    <p class="bg-orange-100 w-96 h-96 text-white p-3 rounded-xl absolute right-0 top-1/3 "></p>
+    <img src="img/kids.png" alt="Kids reading book" class="absolute right-0 max-w-md h-auto mt-8 ">
   </div>
   
