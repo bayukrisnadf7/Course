82a41da6a992b2f2c26c0cfb2be5faa792337355
Bayu Krisna <bayukrisna231@gmail.com>
2024-11-06 23:09:58 +0700

    pembaruan section home

---
+++ resources/views/home/section-5.blade.php
@@ -0,0 +1,11 @@
+<div class="flex flex-col">
+    <div class="flex text-center flex-col gap-5">
+        <p class="text-5xl text-center font-bold">What's in the class at Edudu?</p>
+        <div class="text-xl text-slate-600">
+            <p>Online classes with teachers, continuous questions and answers during class if you do</p>
+            <p>not understand. At the end of the session, the lesson is recorded for your child to review</p>
+        </div>
+        <button class="bg-primary p-3 rounded-xl text-white w-36 mx-auto mt-5">Free trial lesson</button>
+        <img src="img/Image (1).png" alt="">
+    </div>
+</div>
